94121a292c5964c5626751c1230251a524d18442
Rudy Baer <baer.rudy@gmail.com>
2016-01-05 10:56:11 +0100

    ex: use services
    add jokeController
    use $http
    add interactions

--- html/index.js
+++ html/index.js
@@ -11,9 +11,17 @@ app.run(function(localStorageService){
 
 });
 
-app.controller("main", function ($scope) {
+app.controller("main", ['$scope', '$http', function ($scope, $http) {
         $scope.name = "Kevin";
         $scope.jokes= [];
+
+        $http.get('api/joke')
+            .success(function (data) {
+                $scope.jokes = data;
+            }).
+            error(function (data, status, headers, config) {
+                console.log(data);
+            });
 
         $scope.addJoke = function (joke) {
             console.log(joke);
@@ -22,9 +30,49 @@ app.controller("main", function ($scope) {
             var j = {};
             j.txt = joke;
             j.date = new Date();
-            $scope.jokes.push(j);
-            $scope.joke = "";
+            $http.post('api/joke', j)
+                .success(function (data) {
+                    $scope.jokes.push(j);
+                    $scope.joke = "";
+                }).
+                error(function (data, status, headers, config) {
+                    console.log(data);
+                });
         }
 
-    }
+    }]
+);
+
+app.controller("jokeController", ['$scope', '$http', function ($scope, $http) {
+        $scope.addFavorite = function (joke) {
+            joke.favorite = true;
+            updateJoke(joke);
+        }
+
+        $scope.removeFavorite = function (joke) {
+            joke.favorite = false;
+            updateJoke(joke);
+        }
+
+        $scope.plusOne = function (joke) {
+
+            if (joke.score == undefined) {
+                joke.score = 0;
+
+            }
+            joke.score += 1;
+            updateJoke(joke);
+        }
+
+
+        var updateJoke = function (joke) {
+            $http.put('api/joke', joke)
+                .success(function (data) {
+
+                }).
+                error(function (data, status, headers, config) {
+                    console.log(data);
+                });
+        }
+    }]
 );
